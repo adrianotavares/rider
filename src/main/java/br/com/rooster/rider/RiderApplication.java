4a13d5c983dd49773009bc4e5f58efbfecbe5543
adrianotavares <adriano.tavares@gmail.com>
2022-08-21 18:57:41 -0300

    refactoring

--- src/main/java/br/com/rooster/rider/RiderApplication.java
+++ src/main/java/br/com/rooster/rider/RiderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RiderApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(RiderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RiderApplication.class, args);
+    }
 }
--- src/test/java/br/com/rooster/rider/RiderApplicationTests.java
+++ src/test/java/br/com/rooster/rider/RiderApplicationTests.java
@@ -1,7 +1,5 @@
 package br.com.rooster.rider;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,20 +8,22 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class RiderApplicationTests {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@Test
-	void getRiders() throws Exception {
+    @Test
+    void getRiders() throws Exception {
 
-		mvc.perform(MockMvcRequestBuilders
-				.get("/riders")
-				.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
-	}
+        mvc.perform(MockMvcRequestBuilders
+                .get("/riders")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
 }
