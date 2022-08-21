f42fde5aaa1e09cb949ecaa26e4e72519d881e0f
adrianotavares <adriano.tavares@gmail.com>
2022-08-21 18:57:41 -0300

    refactoring

--- src/test/java/br/com/rooster/rider/RiderApplicationTests.java
+++ src/test/java/br/com/rooster/rider/RiderApplicationTests.java
@@ -1,29 +1,27 @@
 package br.com.rooster.rider;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class RiderApplicationTests {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@Test
-	void getRiders() throws Exception {
+    @Test
+    void getRiders() throws Exception {
 
-		mvc.perform(MockMvcRequestBuilders
-				.get("/riders")
-				.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
-	}
+        mvc.perform(get("/riders")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
 }
