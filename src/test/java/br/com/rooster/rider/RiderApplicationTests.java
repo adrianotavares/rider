8420b8045c323acd38660ec5e7e8d182ace8b232
adrianotavares <adriano.tavares@gmail.com>
2022-08-21 19:30:19 -0300

    refactoring

--- src/test/java/br/com/rooster/rider/RiderApplicationTests.java
+++ src/test/java/br/com/rooster/rider/RiderApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -21,6 +22,7 @@ class RiderApplicationTests {
 
         mvc.perform(get("/riders")
                 .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
                 .andExpect(status().isOk());
     }
 
